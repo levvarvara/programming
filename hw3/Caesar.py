adfae95badae20d04205c2e9ae12074c768f7020
levvarvara <31654728+levvarvara@users.noreply.github.com>
2017-11-16 00:21:32 +0300

    Delete Caesar.py

--- hw3/Caesar.py
+++
@@ -1,9 +0,0 @@
-#Шифр Цезаря
-word = input("Введите слово: ")
-for i in word:
-    if i != "я" and i != "z":
-        print(chr(ord(i) + 1), end = "")
-    elif i == "я":
-        print("a", end = "")
-    elif i == "z":
-        print("a", end = "")
