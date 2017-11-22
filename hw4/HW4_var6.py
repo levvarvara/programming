f3b4357aca037def9807bee9783c43c3e01a578f
levvarvara <31654728+levvarvara@users.noreply.github.com>
2017-11-22 13:04:15 +0300

    Add files via upload

---
+++ hw4/HW4_var6.py
@@ -0,0 +1,9 @@
+with open("text.txt", encoding="utf-8") as f:
+    text = f.read()
+    words = text.split()
+counter = 0
+for i in words:
+    if i.istitle():
+        counter +=1
+result = counter/len(words)*100
+print("Процент слов, начинающихся с заглавной буквы: "+"%.2f" % result+"%")
