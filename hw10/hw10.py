03a03dbf35b38c01fc422f6fd052b3a6a69b33ce
levvarvara <31654728+levvarvara@users.noreply.github.com>
2018-04-01 22:16:04 +0300

    Add files via upload

---
+++ hw10/hw10.py
@@ -0,0 +1,26 @@
+import re
+def get_text():
+    with open('wiki2.html', encoding='utf-8') as f:
+        text = f.read()
+    return text
+
+def regular(text):
+    res = re.search(r'>Отряд.*?<a.*?>(.*?)</a>',text)
+    res = res.group(1)
+
+    return res
+
+def to_file(res):
+    with open("отряды.txt", "a", encoding="utf-8") as f:
+        f.write(res+'\n')
+
+def main():
+    res = regular(get_text())
+    to_file(res)
+    with open("отряды.txt", encoding="utf-8") as f:
+        text = f.read()
+    print(text.strip())
+
+
+if __name__ == '__main__':
+    main()
